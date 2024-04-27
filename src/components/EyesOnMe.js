b68290569f813471d99fbec451665df334b50d41
Edmound Njoori <eddnjoori@gmail.com>
2024-04-27 22:24:04 +0300

    Solves the test

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+  function handleFocus(e) {
+    e.preventDefault();
+    console.log("Good!");
+  }
+
+  function handleBlur(e) {
+    e.preventDefault();
+    console.log("Hey! Eyes on me!");
+  }
+  return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>
+      Eyes on me
+    </button>
+  );
+}
+export default EyesOnMe;
